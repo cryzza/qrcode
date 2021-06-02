5fc91c5b5e312f69615d7f7c7581d7da2f0cfa9f
drew <drewwilson2022@gmail.com>
2021-06-01 21:57:55 -0400

    connected api, need to fix images

--- index.js
+++ index.js
@@ -7,28 +7,50 @@ const {
     AwesomeQR
 } = require("awesome-qr");
 const fs = require("fs");
+const CoinGecko = require('coingecko-api');
+const request = require('request')
+
+const CoinGeckoClient = new CoinGecko();
 
 app.get('/api/v1/:coin/:wallet', async (req, res) => {
+    const allCoinsAPI = await CoinGeckoClient.coins.list();
+    const allCoins = allCoinsAPI.data
+    var coinId;
+
+    for (i = 0; i < allCoins.length; i++) {
+        if (allCoins[i].id == req.params.coin) {
+            coinId = allCoins[i].id
+            break
+        }
+    }
+
+    if (!coinId) {
+        return res.send('coin is fake')
+    }
+
+    const theCoin = await CoinGeckoClient.coins.fetch(coinId, {});
+    const logoFull = theCoin.data.image.large
+
     const coin = req.params.coin;
     const wallet = req.params.wallet;
     const code = `assets/qr/${coin}_${wallet}_${Math.random()}_${Date.now()}.png`;
     const codeFull = path.join(__dirname, `/${code}`);
-    const logoFull = path.join(__dirname, `/assets/logos/${coin}.png`);
 
     try {
-        const buffer = await new AwesomeQR({
-            text: wallet,
-            size: 1000,
-            logoImage: fs.readFileSync(logoFull),
-            logoCornerRadius: 0,
-            logoScale: 0.3
-        }).draw();
+        request.get(logoFull, async function (err, res, body) {
+            const buffer = await new AwesomeQR({
+                text: wallet,
+                size: 1000,
+                logoImage: body,
+                logoCornerRadius: 0,
+                logoScale: 0.3
+            }).draw();
 
-        fs.writeFileSync(codeFull, buffer);
+            fs.writeFileSync(codeFull, await buffer);
+        });
 
-        res.sendFile(codeFull, code);
     } catch (err) {
-        res.send('coin doesnt exist, or query too long')
+        res.send('coin doesnt exist, or query too long: ' + err)
     }
 })
 
